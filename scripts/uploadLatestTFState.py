7dc857622c65c26c4b4c970a2834e3648629fb84
ganesha87 <ganeshvarma.cse@gmail.com>
2023-03-07 10:29:00 +0530

    corrected teh bucket path

--- scripts/uploadLatestTFState.py
+++ scripts/uploadLatestTFState.py
@@ -3,4 +3,4 @@ import boto3
 bucketName = 'tf-states-demo'
 s3 = boto3.resource('s3')
 
-s3.meta.client.upload_file('./terraform/terraform.tfstate',bucketName, 'permissionsets/terraform.tfstate')
+s3.meta.client.upload_file('./terraform/terraform.tfstate',bucketName, 'assignments/terraform.tfstate')
